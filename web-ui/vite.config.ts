d26fb973703b55c14a2b6ad87fc6e1ff9fb04c80
lihongwu <v_lihw@digitalgd.com.cn>
2026-07-16 19:13:30 +0800

    chore: add production deploy scripts

--- web-ui/vite.config.ts
+++ web-ui/vite.config.ts
@@ -4,8 +4,11 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
+const appBase = process.env.VITE_BASE || "/";
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: appBase,
   plugins: [vue(), tailwindcss()],
   server: {
     host: "0.0.0.0",
